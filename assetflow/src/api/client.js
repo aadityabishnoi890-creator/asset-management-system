462314e0694c2fb7b14a6043d42c585a96a27c95
Naman Awana <namanawana000@gmail.com>
2026-06-11 14:55:19 +0530

    error fixed

--- assetflow/src/api/client.js
+++ assetflow/src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import toast from 'react-hot-toast'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: '/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
